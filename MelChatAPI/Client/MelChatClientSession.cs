f80c68ab8dfdcf75adfaef11d66464a8de4bd35a
Asus <mehrbodmollakazemi81@gmail.com>
2025-08-31 23:28:50 +0330

    Working on Client Session

---
+++ MelChatAPI/Client/MelChatClientSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelChatAPI.Client
+{
+    public class MelChatClientSession(string username, string passwordHash, string melChatServerAddress)
+    {
+        public string Username { get; private set; } = username;
+        private string PasswordHash { get; set; } = passwordHash;
+        public string MelChatServerAddress { get; private set; } = melChatServerAddress;
+
+        private TcpClient? tcpClient = null;
+
+        public async Task ConnectToServer(CancellationToken cancellationToken = default)
+        {
+            
+        }
+    }
+}
